e513020b6a756288f391f80fa77c106601a70f85
Diego Marcilio <diego.marcilio@uber.com>
2022-09-06 17:20:33 +0200

    dot exporting grouping rules

--- polyglot/piranha/src/utilities/edges_to_dot.py
+++ polyglot/piranha/src/utilities/edges_to_dot.py
@@ -9,15 +9,29 @@ class Edge:
     to: str
     scope: str
 
-# TODO: take file as argument
-f = '../cleanup_rules/java/edges.toml'
+# TODO: take files as argument
+rules_file = '../cleanup_rules/java/rules.toml'
+rules_toml_dict = toml.load(rules_file)
+rules_by_group_dict: dict[str, list[str]] = {}
 
-edges_dict = toml.load(f)
+for rule_toml in rules_toml_dict['rules']:
+    if 'groups' in rule_toml:
+        rule_name: str = rule_toml['name']
+        for group_name in rule_toml['groups']:
+            if group_name in rules_by_group_dict:
+                rules_by_group_dict[group_name].append(rule_name) 
+            else:
+                rules_by_group_dict[group_name] = [rule_name]
 
+for item in rules_by_group_dict.items():
+    print(item)
+
+edges_file = '../cleanup_rules/java/edges.toml'
+edges_toml_dict = toml.load(edges_file)
 # dict's are ordered
 graph_dict: dict[str, list[Edge]] = {}
 
-for edge_toml in edges_dict['edges']:
+for edge_toml in edges_toml_dict['edges']:
     from_node = edge_toml['from']
     to_nodes: list[str] = edge_toml['to']
     scope = edge_toml['scope']
@@ -35,14 +49,17 @@ dot_lines: list[str] = []
 dot_lines.append('digraph {\n')
 
 for node in graph_dict.keys():
-    dot_lines.append('  '  + node)
+    if node in rules_by_group_dict:
+        node_label = '[shape=record label="' +  node + '\\n\\n' + '\\n'.join(rules_by_group_dict[node]) + '"]'
+        dot_lines.append(f'   {node} {node_label}')
+    else: # a rule not in a group 
+        dot_lines.append('  '  + node)
 
 dot_lines.append('\n')
 
 for node, edges in graph_dict.items():
     for edge in edges:
         dot_lines.append('  ' + f'{node} -> {edge.to} [label="{edge.scope}"]')
-
 
 dot_lines.append('\n}')
 
