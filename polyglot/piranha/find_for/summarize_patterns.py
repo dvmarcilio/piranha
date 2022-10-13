0a828227a6e44122156f433d18a3ffde201b1c38
diego.marcilio <diego.marcilio@uber.com>
2022-10-13 07:45:07 +0000

    Summarize patterns with links

--- polyglot/piranha/find_for/summarize_patterns.py
+++ polyglot/piranha/find_for/summarize_patterns.py
@@ -4,10 +4,18 @@ import argparse
 import glob
 import sys
 
-from analysis_dataclasses import Pattern, dataclass_from_dict
+from analysis_dataclasses import FuncLiteral, Pattern, Range, dataclass_from_dict
+
+# Python >= 3.9 has removeprefix
+def remove_prefix(text, prefix):
+    if text.startswith(prefix):
+        return text[len(prefix):]
+    return text
 
 parser = argparse.ArgumentParser()
 parser.add_argument('output_path', type=str)
+parser.add_argument('--sourcegraph-url', type=str, default='', dest='sourcegraph_url')
+parser.add_argument('--prefix-to-remove', type=str, default='/home/user/go-code/', dest='prefix_to_remove')
 args = parser.parse_args()
 
 base_output_path = os.path.abspath(args.output_path) + '/'
@@ -33,6 +41,25 @@ for json_file_path in glob.glob(matches_path + '/*.json'):
 
 total = len(basic) + len(a1) + len(a2)
 
+def sourcegraphify(file_path: str, pattern: Pattern) -> 'list[str]':
+    file_url = remove_prefix(file_path, args.prefix_to_remove)
+    url = args.sourcegraph_url + file_url
+    urls: 'list[str]' = []
+    for func_literal_dict in pattern.m_decl.func_literals:
+        func_literal = dataclass_from_dict(FuncLiteral, func_literal_dict)
+        suffix = '?subtree=true#L' + str(func_literal.start_line())
+        urls.append(url + suffix)
+
+    return urls
+
+def channel_receive_lines(pattern: Pattern) -> 'list[int]':
+    lines: 'list[int]' = []
+    for ch_receive_range_dict in pattern.m_decl.channel_receive_after:
+        rg: Range = dataclass_from_dict(Range, ch_receive_range_dict)
+        lines.append(rg.start_line())
+
+    return lines
+
 with open(base_output_path + 'summary.txt', 'w') as summary_f:
 
     sys.stdout = summary_f
@@ -43,8 +70,14 @@ with open(base_output_path + 'summary.txt', 'w') as summary_f:
         print(f'# {name}: {len(patterns)}')
         for p in patterns:
             print()
-            print(f'    file: {p.m_decl.file}')
+            file_path: str = p.m_decl.file
+            print(f'    file: {file_path}')
             print(f'    func line: {p.m_decl.start_line()}')
+            print(f'    channel receive lines: {channel_receive_lines(p)}')
+            if args.sourcegraph_url:
+                print('    func literals:')
+                for url in sourcegraphify(file_path, p):
+                    print(f'        {url}')
 
         print()
 
